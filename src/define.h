a2fb677b83c61d879a5537bb079c5e1381a30f9d
ricardo2perez <rperezperez@danielcastelao.org>
2021-05-12 00:42:18 +0200

    include define

---
+++ src/define.h
@@ -0,0 +1,36 @@
+// Select either ASCII or the normal binary serial packet type
+
+
+//#define SERIAL_ASCII
+//#define SERIAL_BINARY
+#define DEBUG_SERIAL true
+
+
+
+
+
+#define DEFAULT_BAUDRATE 115200
+
+
+// Commands:
+#define HELP        0x00
+#define INFO        0x01
+#define GET         0x02
+#define SET         0x03
+#define MANUAL      0x04
+
+
+// Parameters:
+
+#define MEMORY      0x11
+#define TIMEOUT     0x12
+#define OVERLOAD    0x13
+#define BOUNCE_OC   0x14
+#define BOUNCE_RF   0x15
+#define BOUNCE_FT   0x16
+#define OPEN        0x17
+#define CLOSE       0x18
+#define STOP        0x19
+
+
+
